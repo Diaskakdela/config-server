c3321d7ac94033bd4669a1732085a2962c9b1554
diasospanov <diaskakdela@gmail.com>
2023-04-24 11:01:15 +0600

    first commit

---
+++ src/test/java/kz/ospanov/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.ospanov.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
